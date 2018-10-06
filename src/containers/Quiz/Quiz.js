279dc05e91d8018b787b276d84adcfde30ad5a82
Aleksey Belchenkov <u608110@gmail.com>
2018-10-06 16:20:24 +0300

    Изменение вопроса

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,8 +5,10 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component {
     state = {
+        activeQuestion: 0,
         quiz: [
             {
+                id: 1,
                 question: 'Какой самый лучший язык программирования для веба?',
                 rightAnswerId: 1,
                 answers: [
@@ -15,12 +17,27 @@ class Quiz extends Component {
                     {id: 3, text: 'Python'},
                     {id: 4, text: 'Ruby'}
                 ]
+            },
+            {
+                id: 2,
+                question: 'FTP - это:',
+                rightAnswerId: 3,
+                answers: [
+                    {id: 1, text: 'почтовый клиент'},
+                    {id: 2, text: 'программа IP-телефонии'},
+                    {id: 3, text: 'протокол передачи файлов'},
+                    {id: 4, text: 'новая модель тостера'}
+                ]
             }
         ]
     };
 
     onAnswerClickHandler = (answerId) => {
         console.log('Answer Id: ', answerId);
+
+        this.setState({
+            activeQuestion: this.state.activeQuestion + 1
+        });
     };
 
     render () {
@@ -29,9 +46,11 @@ class Quiz extends Component {
                 <div className={classes.QuizWrapper}>
                     <h1>Ответьте на все вопросы</h1>
                     <ActiveQuiz
-                        answers={this.state.quiz[0].answers}
-                        question={this.state.quiz[0].question}
+                        answers={this.state.quiz[this.state.activeQuestion].answers}
+                        question={this.state.quiz[this.state.activeQuestion].question}
                         onAnswerClick={this.onAnswerClickHandler}
+                        quizLength={this.state.quiz.length}
+                        answerNumber={this.state.activeQuestion + 1}
                     />
                 </div>
             </div>
