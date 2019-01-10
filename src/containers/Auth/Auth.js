03cdbbd2102145159a61d32317074b872df99741
Aleksey Belchenkov <u608110@gmail.com>
2019-01-10 07:39:14 +0300

    Page of Navigation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
+import classes from './Auth.css';
+
+import Button from '../../components/UI/Button/Button';
 
 class Auth extends Component {
+    loginHandler = () => {
+
+    };
+
+    registerHandler = () => {
+
+    };
+
+    submitHandler = (e) => {
+        e.preventDefault();
+    };
+
+
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <div>
+                            <input type="text" />
+                            <input type="text" />
+                        </div>
+                        <div className={classes.AuthButtonsBlock}>
+                            <Button type="success" onClick={this.loginHandler}>Войти</Button>
+                            <Button type="primary" onClick={this.registerHandler}>Зарегистрироваться</Button>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
