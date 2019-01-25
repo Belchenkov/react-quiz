c73c3bc587bc3036e09b65817467b804f50f7e6d
Aleksey Belchenkov <u608110@gmail.com>
2019-01-25 06:52:01 +0300

    Refactor Page Test on Redux

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,4 +1,13 @@
-import {FETCH_QUIZ_SUCCESS, FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS} from "../actions/actionTypes";
+import {
+    FETCH_QUIZ_SUCCESS,
+    FETCH_QUIZES_ERROR,
+    FETCH_QUIZES_START,
+    FETCH_QUIZES_SUCCESS,
+    FINISH_QUIZ,
+    QUIZ_NEXT_QUESTION,
+    QUIZ_RETRY,
+    QUIZ_SET_STATE
+} from "../actions/actionTypes";
 
 const initialState = {
     quizes: [],
@@ -35,6 +44,31 @@ export default function quizReducer(state = initialState, action) {
                 ...state,
                 loading: false,
                 error: action.error
+            };
+        case QUIZ_SET_STATE:
+            return {
+                ...state,
+                results: action.results,
+                answerState: action.answerState
+            };
+        case FINISH_QUIZ:
+            return {
+                ...state,
+                isFinished: true
+            };
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state,
+                answerState: null,
+                activeQuestion: action.number
+            };
+        case QUIZ_RETRY:
+            return {
+                ...state,
+                activeQuestion: 0,
+                answerState: null,
+                isFinished: false,
+                results: {}
             };
         default:
             return state;
