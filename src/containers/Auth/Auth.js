180567776955e2cd6ca29c0578a6ff87ee03145f
Aleksey Belchenkov <u608110@gmail.com>
2019-01-26 18:31:20 +0300

    Auth in Redix

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import is from 'is_js';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
 import classes from './Auth.css';
 
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
+import {auth} from "../../store/actions/auth";
 
 class Auth extends Component {
     state = {
@@ -38,34 +39,20 @@ class Auth extends Component {
       }
     };
 
-    loginHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        };
-
-        try {
-            const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCLP7Se0oD4LqhwSuuNp6OkC1uLh2OxpAE', authData);
-            console.log(response);
-        } catch (err) {
-            console.error(err);
-        }
+    loginHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            true
+        );
     };
 
-    registerHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        };
-
-        try {
-            const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCLP7Se0oD4LqhwSuuNp6OkC1uLh2OxpAE', authData);
-            console.log(response);
-        } catch (err) {
-           console.error(err);
-        }
+    registerHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            false
+        );
     };
 
     submitHandler = (e) => {
@@ -166,4 +153,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+function mapDispatchToProps(dispatch) {
+    return {
+        auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
