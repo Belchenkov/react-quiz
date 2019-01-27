7b374d3c3ff33eb851bd95295433a475cee388de
Aleksey Belchenkov <u608110@gmail.com>
2019-01-27 11:35:33 +0300

    Generate Route for Auth User

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import classes from './Layout.css';
+import connect from "react-redux/es/connect/connect";
 
 import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
 import Drawer from "../../components/Navigation/Drawer/Drawer";
@@ -26,6 +27,7 @@ class Layout extends Component {
         return (
             <div className={classes.Layout}>
                 <Drawer
+                    isAuthenticated={this.props.isAuthenticated}
                     isOpen={this.state.menu}
                     onClose={this.menuCloseHandler}
                 />
@@ -42,4 +44,10 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.auth.token
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
